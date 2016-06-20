d816988823d2b7f78bf33e6e08ccf2589904a770
Ackara <ackara.dev@outlook.com>
2016-06-20 07:19:11 -0400

    Apply minor changes

--- src/Plaid.Core/Properties/AssemblyInfo.cs
+++ src/Plaid.Core/Properties/AssemblyInfo.cs
@@ -7,9 +7,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Plaid.Core")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A portable class library that allows you to retrieve banking data using the Plaid RESTful API.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Gigobyte")]
 [assembly: AssemblyProduct("Plaid.Core")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.16172.4")]
+[assembly: AssemblyFileVersion("1.0.16172.4")]
