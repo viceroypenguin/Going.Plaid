97554868d0fa65aadfe485166d87dc5a916567bb
Zain Ahmad Khan <niazi_zain@hotmail.com>
2019-02-03 13:44:05 +0500

    Unofficial currency code added in account balances

--- src/Plaid/Entity/Balance.cs
+++ src/Plaid/Entity/Balance.cs
@@ -34,5 +34,13 @@ namespace Acklann.Plaid.Entity
         /// <value>currency code.</value>
         [JsonProperty("iso_currency_code")]
         public string ISOCurrencyCode { get; set; }
+
+        /// <summary>
+        /// Gets or sets the unofficial currency code.
+        /// </summary>
+        /// <value>currency code.</value>
+        [JsonProperty("unofficial_currency_code")]
+        public string UnofficialCurrencyCode { get; set; }
+
     }
 }
