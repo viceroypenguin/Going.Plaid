fbbadbd52c00359533bbcd97ef5ed5f0e436cdde
Zain Ahmad Khan <niazi_zain@hotmail.com>
2019-02-04 13:26:35 +0500

    Getting stripe account token using Plaid access token. Reference to this page https://plaid.com/docs/stripe/

---
+++ src/Plaid/Management/StripeTokenRequest.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Acklann.Plaid.Management
+{
+    /// <summary>
+    /// Represents a request for plaid's '/processor/stripe/bank_account_token/create' endpoint. Exchange a Link access_token for an Stripe API stripe_bank_account_token and request_id. 
+    /// </summary>
+    /// <seealso cref="Acklann.Plaid.SerializableContent" />
+    public class StripeTokenRequest : SerializableContent
+    {
+        /// <summary>
+        /// Gets or sets the access_token.
+        /// </summary>
+        /// <value>The access token.</value>
+        [JsonProperty("access_token")]
+        public string AccessToken { get; set; }
+
+        /// <summary>
+        /// Gets or sets the client identifier.
+        /// </summary>
+        /// <value>The client identifier.</value>
+        [JsonProperty("client_id")]
+        public string ClientId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the secret.
+        /// </summary>
+        /// <value>The secret.</value>
+        [JsonProperty("secret")]
+        public string Secret { get; set; }
+
+
+        /// <summary>
+        /// Gets or sets the account id.
+        /// </summary>
+        /// <value>The account id.</value>
+        [JsonProperty("account_id")]
+        public string AccountId { get; set; }
+    }
+}
