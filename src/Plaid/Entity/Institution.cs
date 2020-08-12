c074818d7672c6e3de369d88eb309d828e5ac354
Jeff Bacon <jeffbacon@tshconsulting.ca>
2020-08-12 19:06:33 -0400

    Added missing fields to Institutions
    Fixes #4

--- src/Plaid/Entity/Institution.cs
+++ src/Plaid/Entity/Institution.cs
@@ -26,15 +26,68 @@ namespace Going.Plaid.Entity
 		public string[] Products { get; set; } = null!;
 
 		/// <summary>
+		/// A list of routing numbers associated with the institution.
+		/// </summary>
+		[JsonProperty("routing_numbers")]
+		public string[]? RoutingNumbers { get; set; }
+
+		/// <summary>
+		/// A list of country codes associated with the institution.
+		/// </summary>
+		[JsonProperty("country_codes")]
+		public string[]? CountryCodes { get; set; }
+
+		/// <summary>
+		/// Inidcates if this institution uses OAuth flows
+		/// </summary>
+		[JsonProperty("oauth")]
+		public bool? OAuth { get; set; }
+
+		#region MFA
+
+		/// <summary>
+		/// Inidcates if this institution uses MFA
+		/// </summary>
+		[JsonProperty("has_mfa")]
+		public bool? HasMFA { get; set; }
+
+		/// <summary>
+		/// A list of mfa types associated with the institution.
+		/// </summary>
+		[JsonProperty("mfa")]
+		public string[]? MFA { get; set; }
+
+		/// <summary>
+		/// The URL for the institution's website.
+		/// </summary>
+		[JsonProperty("mfa_code_type")]
+		public string? MFACodeType { get; set; }
+
+		#endregion
+
+		#region Optional Metadata: include_optional_metadata
+
+		/// <summary>
 		/// The URL for the institution's website.
 		/// </summary>
 		[JsonProperty("url")]
 		public string? Url { get; set; }
 
 		/// <summary>
-		/// A list of routing numbers associated with the institution.
+		/// The primary colour for the institution to use in visual displays.
+		/// The Brand color is in hexadecimal format
 		/// </summary>
-		[JsonProperty("routing_numbers")]
-		public string[]? RoutingNumbers { get; set; }
+		[JsonProperty("primary_color")]
+		public string? PrimaryColor { get; set; }
+
+		/// <summary>
+		/// The logo for the institution to use in visual displays.
+		/// When available, the bank's logo is returned as a base64 encoded 152x152 PNG
+		/// </summary>
+		[JsonProperty("logo")]
+		public string? Logo { get; set; }
+
+		#endregion
+
 	}
 }
