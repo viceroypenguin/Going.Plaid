073b107535897e04c7c73a7f60b00ddf5abaa9e6
Stuart Turner <stuart@turner-isageek.com>
2020-09-15 08:27:43 -0500

    Update PlaidException to be more accurate to the Plaid error object

---
+++ src/Plaid/Entity/ErrorCode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Going.Plaid.Entity
+{
+	/// <summary>
+	/// The particular error code.
+	/// </summary>
+	public enum ErrorCode
+	{
+	}
+}
--- src/Plaid/Exceptions/PlaidException.cs
+++ src/Plaid/Exceptions/PlaidException.cs
@@ -1,5 +1,7 @@
-﻿using Newtonsoft.Json;
+﻿using Going.Plaid.Entity;
+using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 
 namespace Going.Plaid.Exceptions
 {
@@ -44,24 +46,64 @@ namespace Going.Plaid.Exceptions
 		}
 
 		/// <summary>
-		/// Gets or sets the type of the error. A broad categorization of the error.
+		/// A broad categorization of the error.
 		/// </summary>
-		/// <value>The type of the error.</value>
 		[JsonProperty("error_type")]
-		public string ErrorType { get; set; } = null!;
+		public ErrorType ErrorType { get; set; }
 
 		/// <summary>
-		/// Gets or sets the error code. The particular error code. Each error_type has a specific set
+		/// The particular error code.
 		/// </summary>
-		/// <value>The error code.</value>
 		[JsonProperty("error_code")]
-		public string ErrorCode { get; set; } = null!;
+		public ErrorCode ErrorCode { get; set; }
 
 		/// <summary>
-		/// Gets or sets the display message. A user-friendly representation of the error code. null if the error is not related to user action. This may change over time and is not safe for programmatic use.
+		/// A developer-friendly representation of the error code.
 		/// </summary>
-		/// <value>The display message.</value>
+		/// <remarks>This may change over time and is not safe for programmatic use.</remarks>
+		[JsonProperty("developer_message")]
+		public string DeveloperMessage { get; set; } = null!;
+
+		/// <summary>
+		/// A user-friendly representation of the error code. <c>null</c> if the error is not related to user action.
+		/// </summary>
+		/// <remarks>
+		/// This may change over time and is not safe for programmatic use.
+		/// </remarks>
 		[JsonProperty("display_message")]
-		public string DisplayMessage { get; set; } = null!;
+		public string? DisplayMessage { get; set; }
+
+		/// <summary>
+		/// A unique string identifying the request, to be used for troubleshooting purposes. 
+		/// </summary>
+		/// <remarks>This field will be omitted in errors provided by webhooks.</remarks>
+		[JsonProperty("request_id")]
+		public string? RequestId { get; set; }
+
+		/// <summary>
+		/// In the Assets product, a request can pertain to more than one Item. If an error is returned for such a request, causes will return an array of errors containing a breakdown of these errors on the individual Item level, if any can be identified.
+		/// </summary>
+		/// <remarks><see cref="Causes"/> will only be provided for the error type <see cref="ErrorType.AssetReportError"/>.</remarks>
+		[JsonProperty("causes")]
+		public IReadOnlyList<string> Causes { get; set; } = Array.Empty<string>();
+
+		/// <summary>
+		/// The HTTP status code associated with the error. 
+		/// </summary>
+		/// <remarks>This will only be returned in the response body when the error information is provided via a webhook.</remarks>
+		[JsonProperty("status")]
+		public int? StatusCode { get; set; }
+
+		/// <summary>
+		/// The URL of a Plaid documentation page with more information about the error
+		/// </summary>
+		[JsonProperty("documentation_url")]
+		public string? DocumentationUrl { get; set; }
+
+		/// <summary>
+		/// Suggested steps for resolving the error
+		/// </summary>
+		[JsonProperty("suggested_action")]
+		public string? SuggestedAction { get; set; }
 	}
 }
