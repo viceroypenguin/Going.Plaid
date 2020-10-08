f4132975605574c77110cd5f3bf3279b5db34ddf
Stuart Turner <stuart@turner-isageek.com>
2020-10-08 12:03:46 -0700

    Add Error Code Enums (#12)
    * Update PlaidException to be more accurate to the Plaid error object
    
    * Add error codes for ITEM_ERROR
    
    * Add API_ERRORs and INSTITUTION_ERRORs
    
    * Add INVALID_INPUT codes
    
    * Finish error codes
    
    * Update ResponseBase to match updated Error type
    
    * Correct docs links

--- src/Plaid/Entity/WebhookType.cs
+++ src/Plaid/Entity/WebhookType.cs
@@ -15,7 +15,7 @@ namespace Going.Plaid.Entity
 		/// or errors encountered with an Item. The error typically requires user action to 
 		/// resolve, such as when a user changes their password.
 		/// </summary>
-		/// <remarks><see href="https://plaid.com/docs-beta/api/webhooks/#item-webhooks"/></remarks>
+		/// <remarks><see href="https://plaid.com/docs/api/webhooks/#item-webhooks"/></remarks>
 		[EnumMember(Value = "ITEM")]
 		Item,
 
@@ -24,7 +24,7 @@ namespace Going.Plaid.Entity
 		/// a webhook upon successful verification, 1 to 2 business days later. 
 		/// Occasionally when verification does not succeed, Plaid sends an expired webhook.
 		/// </summary>
-		/// <remarks><see href="https://plaid.com/docs-beta/api/webhooks/#auth-webhooks"/></remarks>
+		/// <remarks><see href="https://plaid.com/docs/api/webhooks/#auth-webhooks"/></remarks>
 		[EnumMember(Value = "AUTH")]
 		Auth,
 
@@ -32,7 +32,7 @@ namespace Going.Plaid.Entity
 		/// Fired whenever there are new transactions associated with an Item, 
 		/// including when Plaid’s initial and historical transaction pull are completed. 
 		/// </summary>
-		/// <remarks><see href="https://plaid.com/docs-beta/api/webhooks/#transaction-webhooks"/></remarks>
+		/// <remarks><see href="https://plaid.com/docs/api/webhooks/#transaction-webhooks"/></remarks>
 		[EnumMember(Value = "TRANSACTIONS")]
 		Transactions,
 
@@ -41,14 +41,14 @@ namespace Going.Plaid.Entity
 		/// The webhook typically fires once per day, after market close, in response to any 
 		/// newly added holdings or price changes to existing holdings.
 		/// </summary>
-		/// <remarks><see href="https://plaid.com/docs-beta/api/webhooks/#holdings-default_update"/></remarks>
+		/// <remarks><see href="https://plaid.com/docs/api/webhooks/#holdings-default_update"/></remarks>
 		[EnumMember(Value = "HOLDINGS")]
 		Holdings,
 
 		/// <summary>
 		/// Fired when new or canceled transactions have been detected on an investment account.
 		/// </summary>
-		/// <remarks><see href="https://plaid.com/docs-beta/api/webhooks/#investments_transactions-default_update"/></remarks>
+		/// <remarks><see href="https://plaid.com/docs/api/webhooks/#investments_transactions-default_update"/></remarks>
 		[EnumMember(Value = "INVESTMENTS_TRANSACTIONS")]
 		InvestmentTransactions,
 	}
