64f967c191e6d4ac0842f525f492c1a7eac84176
Stuart Turner <stuart@turner-isageek.com>
2021-04-10 18:46:31 -0500

    Make converters public

--- src/Plaid/Converters/DateTimeConverter.cs
+++ src/Plaid/Converters/DateTimeConverter.cs
@@ -6,11 +6,14 @@ using System.Text.Json.Serialization;
 
 namespace Going.Plaid.Converters
 {
-	internal class DateTimeConverter : JsonConverter<DateTime>
+	/// <inheritdoc/>
+	public class DateTimeConverter : JsonConverter<DateTime>
 	{
+		/// <inheritdoc/>
 		public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) =>
 			DateTime.Parse(reader.GetString() ?? string.Empty);
 
+		/// <inheritdoc/>
 		public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options) =>
 			writer.WriteStringValue(
 				value.ToString("yyyy-MM-dd"));
