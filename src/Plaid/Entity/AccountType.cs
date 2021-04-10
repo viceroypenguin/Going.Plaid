af87ec933889a2b4beb833eeae41fdbc0d0e6d32
Stuart Turner <stuart@turner-isageek.com>
2021-04-10 14:14:16 -0500

    Improve enum parsing behavior

--- src/Plaid/Entity/AccountType.cs
+++ src/Plaid/Entity/AccountType.cs
@@ -1,4 +1,6 @@
-﻿namespace Going.Plaid.Entity
+﻿using System.Runtime.Serialization;
+
+namespace Going.Plaid.Entity
 {
 	/// <summary>
 	/// Enum AccountType
@@ -8,36 +10,43 @@
 		/// <summary>
 		/// Brokerage account.
 		/// </summary>
+		[EnumMember(Value = "brokerage")]
 		Brokerage,
 
 		/// <summary>
 		/// Credit card.
 		/// </summary>
+		[EnumMember(Value = "credit")]
 		Credit,
 
 		/// <summary>
 		/// Checking or Savings accounts.
 		/// </summary>
+		[EnumMember(Value = "depository")]
 		Depository,
 
 		/// <summary>
 		/// Investment accounts.
 		/// </summary>
+		[EnumMember(Value = "investment")]
 		Investment,
 
 		/// <summary>
 		/// Loan account.
 		/// </summary>
+		[EnumMember(Value = "loan")]
 		Loan,
 
 		/// <summary>
 		/// Mortgage account.
 		/// </summary>
+		[EnumMember(Value = "mortgage")]
 		Mortgage,
 
 		/// <summary>
 		/// Non-specified account type
 		/// </summary>
+		[EnumMember(Value = "other")]
 		Other
 	}
 }
