ce2f391f3407a1de119ec6aff82a1093fc1e2aaa
Stuart Turner <stuart@turner-isageek.com>
2021-05-05 08:30:41 -0500

    Update Institution query APIs (#43)
    * Update Institution query APIs
    
    * Add Institution Status data

--- src/Plaid/Institution/GetAllInstitutionsRequest.cs
+++ src/Plaid/Institution/GetAllInstitutionsRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Text.Json.Serialization;
+using Going.Plaid.Entity;
 using Going.Plaid.Options;
 
 namespace Going.Plaid.Institution
@@ -49,6 +50,42 @@ namespace Going.Plaid.Institution
 			if (string.IsNullOrWhiteSpace(Secret)) Secret = secret;
 			if (string.IsNullOrWhiteSpace(ClientId)) ClientId = clientId;
 			AccessToken = null;
+		}
+
+		/// <summary>
+		/// Options for searching Institutions
+		/// </summary>
+		public struct InstitutionSearchOptions
+		{
+			/// <summary>
+			/// Filter the <see cref="Entity.Institution"/> based on whether they support all products listed in <see cref="Products"/>.
+			/// </summary>
+			[JsonPropertyName("products")]
+			public Product[]? Products { get; set; }
+
+			/// <summary>
+			/// Specify an array of routing numbers to filter institutions.
+			/// </summary>
+			[JsonPropertyName("routing_numbers")]
+			public string[]? RoutingNumbers { get; set; }
+
+			/// <summary>
+			/// Limit results to institutions with or without OAuth login flows. This is only relevant to institutions with European country codes.
+			/// </summary>
+			[JsonPropertyName("oauth")]
+			public bool OAuth { get; set; }
+
+			/// <summary>
+			/// When <c>true</c>, return an institution's logo, brand color, and URL. When available, the bank's logo is returned as a base64 encoded 152x152 PNG, the brand color is in hexadecimal format.
+			/// </summary>
+			[JsonPropertyName("include_optional_metadata")]
+			public bool IncludeOptionalMetadata { get; set; }
+
+			/// <summary>
+			/// When <c>true</c>, returns metadata related to the Payment Initiation product indicating which payment configurations are supported.
+			/// </summary>
+			[JsonPropertyName("include_payment_initiation_metadata")]
+			public bool IncludePaymentInitiationMetadata { get; set; }
 		}
 	}
 }
--- src/Plaid/Institution/SearchRequest.cs
+++ src/Plaid/Institution/SearchRequest.cs
@@ -43,5 +43,47 @@ namespace Going.Plaid.Institution
 			if (string.IsNullOrWhiteSpace(ClientId)) ClientId = clientId;
 			AccessToken = null;
 		}
+
+		/// <summary>
+		/// Options for searching Institutions
+		/// </summary>
+		public struct InstitutionSearchOptions
+		{
+			/// <summary>
+			/// Limit results to institutions with or without OAuth login flows. This is only relevant to institutions with European country codes.
+			/// </summary>
+			[JsonPropertyName("oauth")]
+			public bool OAuth { get; set; }
+
+			/// <summary>
+			/// When <c>true</c>, return an institution's logo, brand color, and URL. When available, the bank's logo is returned as a base64 encoded 152x152 PNG, the brand color is in hexadecimal format.
+			/// </summary>
+			[JsonPropertyName("include_optional_metadata")]
+			public bool IncludeOptionalMetadata { get; set; }
+
+			/// <summary>
+			/// When <c>true</c>, returns metadata related to the Payment Initiation product indicating which payment configurations are supported.
+			/// </summary>
+			[JsonPropertyName("include_payment_initiation_metadata")]
+			public bool IncludePaymentInitiationMetadata { get; set; }
+
+			/// <summary>
+			/// Additional options that will be used to filter institutions by various Payment Initiation configurations.
+			/// </summary>
+			[JsonPropertyName("payment_initiation")]
+			public PaymentInitiationOptions? PaymentInitiationOptions { get; set; }
+		}
+
+		/// <summary>
+		/// Additional options that will be used to filter institutions by various Payment Initiation configurations.
+		/// </summary>
+		public struct PaymentInitiationOptions
+		{
+			/// <summary>
+			/// A unique ID identifying the payment
+			/// </summary>
+			[JsonPropertyName("payment_id")]
+			public string PaymentId { get; set; }
+		}
 	}
 }
